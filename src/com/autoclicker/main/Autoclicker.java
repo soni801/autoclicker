016b832caa91c86afba65aa2a4e3d8113ae389c4
soni801 <32430506+soni801@users.noreply.github.com>
2021-07-23 11:39:39 +0200

    Add time interval UI

--- src/com/autoclicker/main/Autoclicker.java
+++ src/com/autoclicker/main/Autoclicker.java
@@ -10,14 +10,47 @@ import javax.swing.*;
  */
 public class Autoclicker implements NativeKeyListener
 {
+    // Frame
+    JFrame frame;
+    
+    // Time interval
+    JPanel timeIntervalPanel;
+    JLabel timeIntervalLabel;
+    JTextField timeIntervalField;
+    JComboBox<String> timeIntervalUnitSelector;
+    
     public Autoclicker()
     {
-        JFrame frame = new JFrame();
+        // Initialise objects
+        frame = new JFrame();
         
+        timeIntervalPanel = new JPanel();
+        timeIntervalLabel = new JLabel("Time interval:");
+        timeIntervalField = new JTextField(4);
+        timeIntervalUnitSelector = new JComboBox<>();
+        
+        // Add items to ComboBox
+        timeIntervalUnitSelector.addItem("Hours");
+        timeIntervalUnitSelector.addItem("Minutes");
+        timeIntervalUnitSelector.addItem("Seconds");
+        timeIntervalUnitSelector.addItem("Milliseconds");
+        timeIntervalUnitSelector.addItem("Microseconds");
+        timeIntervalUnitSelector.addItem("Nanoseconds");
+        
+        // Add objects to panels
+        timeIntervalPanel.add(timeIntervalLabel);
+        timeIntervalPanel.add(timeIntervalField);
+        timeIntervalPanel.add(timeIntervalUnitSelector);
+        
+        // Add panels to frame
+        frame.add(timeIntervalPanel);
+        
+        // Initialise frame
         frame.setTitle("Soni's Autoclicker");
         frame.setSize(300, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
         frame.setVisible(true);
     }
     
