70be988091b74f3cddefc6148bb54f21a32552e9
soni801 <32430506+soni801@users.noreply.github.com>
2021-07-23 10:58:22 +0200

    Create base

---
+++ src/com/autoclicker/main/Autoclicker.java
@@ -0,0 +1,33 @@
+package com.autoclicker.main;
+
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+/**
+ * @author Soni
+ */
+public class Autoclicker implements NativeKeyListener
+{
+    public static void main(String[] args)
+    {
+    
+    }
+    
+    @Override
+    public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent)
+    {
+    
+    }
+    
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent)
+    {
+    
+    }
+    
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent)
+    {
+    
+    }
+}
